7a0b5c310cd5ecde0fd18da76a774361e9fe9a58
njine <brikab@gmail.com>
2023-10-25 05:17:55 +0300

    104

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,42 +1,42 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
- */
+* swap_ints - Swap two integers in an array.
+* @a: The first integer to swap.
+* @b: The second integer to swap.
+*/
 void swap_ints(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+int tmp = *a;
+*a = *b;
+*b = tmp;
 }
 
 /**
- * shell_sort - Sort an array of integers in ascending
- *              order using the Shell sort algorithm with Knuth sequence.
- * @array: An array of integers.
- * @size: The size of the array.
- */
+* shell_sort - Sort an array of integers in ascending
+*              order using the Shell sort algorithm with Knuth sequence.
+* @array: An array of integers.
+* @size: The size of the array.
+*/
 void shell_sort(int *array, size_t size)
 {
-    size_t gap = 1, i, j;
+size_t gap = 1, i, j;
 
-    if (!array || size < 2)
-        return;
+if (!array || size < 2)
+return;
 
-    while (gap < size / 3)
-        gap = gap * 3 + 1;
+while (gap < size / 3)
+gap = gap * 3 + 1;
 
-    for (; gap >= 1; gap /= 3)
-    {
-        for (i = gap; i < size; i++)
-        {
-            for (j = i; j >= gap && array[j - gap] > array[j]; j -= gap)
-            {
-                swap_ints(&array[j], &array[j - gap]);
-            }
-        }
-        print_array(array, size);
-    }
+for (; gap >= 1; gap /= 3)
+{
+for (i = gap; i < size; i++)
+{
+for (j = i; j >= gap && array[j - gap] > array[j]; j -= gap)
+{
+swap_ints(&array[j], &array[j - gap]);
+}
+}
+print_array(array, size);
+}
 }
--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,38 +1,38 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using Selection Sort
- * @array: The array to be sorted
- * @size: Number of elements in the array
- */
+* selection_sort - Sorts an array of integers in ascending order using Selection Sort
+* @array: The array to be sorted
+* @size: Number of elements in the array
+*/
 void selection_sort(int *array, size_t size)
 {
-    size_t i, j, min_idx;
-    int tmp;
+size_t i, j, min_idx;
+int tmp;
 
-    if (!array || size < 2)
-        return;
+if (!array || size < 2)
+return;
 
-    i = 0;
-    while (i < size - 1)
-    {
-        min_idx = i;
-        j = i + 1;
+i = 0;
+while (i < size - 1)
+{
+min_idx = i;
+j = i + 1;
 
-        while (j < size)
-        {
-            if (array[j] < array[min_idx])
-                min_idx = j;
-            j++;
-        }
+while (j < size)
+{
+if (array[j] < array[min_idx])
+min_idx = j;
+j++;
+}
 
-        if (min_idx != i)
-        {
-            tmp = array[i];
-            array[i] = array[min_idx];
-            array[min_idx] = tmp;
-            print_array(array, size);
-        }
-        i++;
-    }
+if (min_idx != i)
+{
+tmp = array[i];
+array[i] = array[min_idx];
+array[min_idx] = tmp;
+print_array(array, size);
+}
+i++;
+}
 }
