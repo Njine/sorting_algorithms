92956e9eef78c437fd7230dfef3ca404fdd7d6b6
njine <brikab@gmail.com>
2023-10-25 05:09:46 +0300

    104

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,70 +1,68 @@
 #include "sort.h"
+
 /**
- * check_tree - swiftdown check
- * @array: pointer to array
- * @size: size of the pointer
- * @size_init: original size of the array
- * @i: index as a root of the tree
- *
-**/
+ * check_tree - Sift down check
+ * @array: Pointer to array
+ * @size: Size of the pointer
+ * @size_init: Original size of the array
+ * @i: Index as a root of the tree
+ */
 void check_tree(int *array, size_t size_init, size_t size, size_t i)
 {
+	int root_val, left_val, right_val;
+	size_t left_idx, right_idx;
 
-	int n, div1, div2;
-	size_t d1, d2;
+	left_idx = i * 2 + 1;
+	right_idx = left_idx + 1;
+	left_val = array[left_idx];
+	right_val = array[right_idx];
 
-	d2 = i * 2 + 1;
-	d2 = d1 + 1;
-	div1 = array[d1];
-	div2 = array[d2];
-	if (((d1 < size) && (d2 < size) &&
-		(div1 >= div2 && div1 > array[i]))
-		|| ((d1 == size - 1) && div1 > array[i]))
+	if (((left_idx < size) && (right_idx < size) &&
+		(left_val >= right_val && left_val > array[i])
+		|| ((left_idx == size - 1) && left_val > array[i]))
 	{
-		n = array[i];
-		array[i] = div1;
-		array[d1] = n;
+		root_val = array[i];
+		array[i] = left_val;
+		array[left_idx] = root_val;
 		print_array(array, size_init);
 	}
-	else if ((d1 < size) && (d2 < size) &&
-		(div2 > div1 && div2 > array[i]))
+	else if ((left_idx < size) && (right_idx < size) &&
+		(right_val > left_val && right_val > array[i]))
 	{
-		n = array[i];
-		array[i] = div2;
-		array[d2] = n;
+		root_val = array[i];
+		array[i] = right_val;
+		array[right_idx] = root_val;
 		print_array(array, size_init);
 	}
-	if (d1 < size - 1)
-		check_tree(array, size_init, size, d1);
-	if (d2 < size - 1)
-		check_tree(array, size_init, size, d2);
+	if (left_idx < size - 1)
+		check_tree(array, size_init, size, left_idx);
+	if (right_idx < size - 1)
+		check_tree(array, size_init, size, right_idx);
 }
+
 /**
- * heap_sort - sorts an array of integers
- * in ascending order using the Heap
- * sort algorithm
- * @array: pointer to array
- * @size: size of the pointer
- *
-**/
+ * heap_sort - Sorts an array of integers in ascending order
+ * using the Heap sort algorithm
+ * @array: Pointer to array
+ * @size: Size of the pointer
+ */
 void heap_sort(int *array, size_t size)
 {
 	size_t i, size_init = size;
-	int n;
+	int temp;
 
 	if (!array)
 		return;
-	for (i = 0; i < size / 2 ; i++)
+	for (i = 0; i < size / 2; i++)
 	{
 		check_tree(array, size_init, size, size / 2 - 1 - i);
 	}
 	for (i = 0; i < size_init - 1; i++)
 	{
-		n = array[0];
+		temp = array[0];
 		array[0] = array[size - 1 - i];
-		array[size - 1 - i] = n;
+		array[size - 1 - i] = temp;
 		print_array(array, size_init);
 		check_tree(array, size_init, size - i - 1, 0);
 	}
-
 }
