a382cdd6d77c5d35d1177ad6d71c79f43e1f8acc
njine <brikab@gmail.com>
2023-10-25 05:23:00 +0300

    cric

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,10 +2,10 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array of integers in ascending order using the
+ * selection_sort - sorts array of integers in ascending order with the
  *			Selection sort algorithm
  * @array: array to sort
- * @size: size of the array
+ * @size: size of  array
  */
 void selection_sort(int *array, size_t size)
 {
