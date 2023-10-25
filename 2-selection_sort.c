cbbf38fe05363887b40a2015cb96f0fa336f0e83
njine <brikab@gmail.com>
2023-10-25 05:21:41 +0300

    cric

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,38 +1,34 @@
+#include <stdio.h>
 #include "sort.h"
 
 /**
-* selection_sort - Sorts an array of integers in ascending order using Selection Sort
-* @array: The array to be sorted
-* @size: Number of elements in the array
-*/
+ * selection_sort - sorts an array of integers in ascending order using the
+ *			Selection sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ */
 void selection_sort(int *array, size_t size)
 {
-size_t i, j, min_idx;
-int tmp;
+	size_t i, j, min_idx;
+	int tmp;
 
-if (!array || size < 2)
-return;
+	if (array == NULL || size < 2)
+		return;
 
-i = 0;
-while (i < size - 1)
-{
-min_idx = i;
-j = i + 1;
-
-while (j < size)
-{
-if (array[j] < array[min_idx])
-min_idx = j;
-j++;
-}
-
-if (min_idx != i)
-{
-tmp = array[i];
-array[i] = array[min_idx];
-array[min_idx] = tmp;
-print_array(array, size);
-}
-i++;
-}
+	for (i = 0; i < size - 1; i++)
+	{
+		min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+		if (min_idx != i)
+		{
+			tmp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = tmp;
+			print_array(array, size);
+		}
+	}
 }
